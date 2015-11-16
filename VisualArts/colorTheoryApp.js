716f34d8df34d07013d903373cb0d217b28d30ed
Tim Adam <tdadam@xactware.com>
2015-11-16 10:12:29 -0700

    Added js function to adjust the array value without refreshing

--- VisualArts/colorTheoryApp.js
+++ VisualArts/colorTheoryApp.js
@@ -2,19 +2,20 @@
  * Created by kris on 11/16/15.
  */
 
-var questionNumber, colorValue=['DarkOrange', 'purple', 'green', 'black'], answer, box1=['red', 'blue', 'yellow'], box2=['black', 'blue', 'yellow', 'red'], box1Color, box2Color, box1Value;
-
+var questionNumber, colorValue=['DarkOrange', 'purple', 'green', 'black'], answer, box1=['red', 'blue', 'yellow'], box2=['black', 'blue', 'yellow', 'red'], box1Color, box2Color, box1Value, box2Value;
 
 function loadColors(n) {
     box1Value = Math.floor(Math.random() * 3);
+    box2Value = (box1Value + 1);
     $('#box1').css("background-color", box1[box1Value]); box1Color=box1[box1Value];
-    $('#box2').css("background-color", box2[(box1Value + 1)]); box2Color=box2[(box1Value + 1)];
+    $('#box2').css("background-color", box2[(box2Value)]); box2Color=box2[(box2Value)];
     answer = colorValue[1];
 
     $('#colorValue1').css("background-color", colorValue[0]);
     $('#colorValue2').css("background-color", colorValue[1]);
     $('#colorValue3').css("background-color", colorValue[2]);
     $('#colorValue4').css("background-color", colorValue[3]);
+    randomizer();
 }
 
 function checkColor(n) {
@@ -41,7 +42,18 @@ function checkColor(n) {
 }
 
 function randomizer() {
-
+    if (box1Value < 2) {
+        box1Value++;
+    }
+    else if (box1Value == 2) {
+        box1Value = 0;
+    }
+    if (box2Value < 2) {
+        box2Value += 2;
+    }
+    else if (box2Value >= 2){
+        box2Value -= 2;
+    }
 }
 
 
