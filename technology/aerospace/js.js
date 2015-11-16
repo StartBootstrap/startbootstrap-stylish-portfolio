1125da1e79664c13a07d1729dd31218a22c09d7f
Wesley Young <wesley.young.portfolio@gmail.com>
2015-11-16 10:40:56 -0700

    Tuned up

--- technology/aerospace/js.js
+++ technology/aerospace/js.js
@@ -2,14 +2,18 @@
  * Created by zacharyhuffman on 11/16/15.
  */
 var current = 0;
-var answers = "acdba";
+var answers = "acdbabdab";
 var response = "";
 var finished = [];
 var questions = {q1: {description: "Fuselage", pic: "images/fuselage.jpg", id: 1, answer: "Fuselage", a: "Fuselage", b: "Wheel", c: "Wing", d: "Cockpit"},
     q2: {description: "Wings", pic: "images/wing.jpg", id: 2, answer: "Wings", a: "Cabin", b: "Window", c: "Wings", d: "Tail"},
     q3: {description: "Engine", pic: "images/engine.jpg", id: 3, answer: "Engine", a: "Wheel", b: "Fuselage", c: "Elevator", d: "Engine"},
     q4: {description: "Horizontal Stabilizer", pic: "images/horizontal_stabilizer.JPG", id: 4, answer: "Horizontal Stabilizer", a: "Tail", b: "Horizontal Stabilizer", c: "Vertical Stabilizer", d: "Wings"},
-    q5: {description: "Vertical Stabilizer", pic: "images/vertical_stabilizer.jpg", id: 5, answer: "Vertical Stabilizer", a: "Vertical Stabilizer", b: "Cabin", c: "Aileron", d: "Elevator"}};
+    q5: {description: "Vertical Stabilizer", pic: "images/vertical_stabilizer.jpg", id: 5, answer: "Vertical Stabilizer", a: "Vertical Stabilizer", b: "Cabin", c: "Aileron", d: "Elevator"},
+    q6: {description: "Aileron", pic: "images/aileron.jpg", id: 6, answer: "Aileron", a: "Horizontal Stabilizer", b: "Aileron", c: "Pilot", d: "Wheels"},
+    q7: {description: "Elevator", pic: "images/elevator.JPG", id: 7, answer: "Aileron", a: "Aileron", b: "Fuselage", c: "Window", d: "Elevator"},
+    q8: {description: "Cockpit", pic: "images/cockpit.jpg", id: 8, answer: "Cockpit", a: "Cockpit", b: "Vertical Stabilizer", c: "Window", d: "Horizontal Stabilizer"},
+    q9: {description: "Roll", pic: "images/roll.gif", id: 9, answer: "Roll", a: "Yaw", b: "Roll", c: "Pitch", d: "None of the above"}};
 var selected = [];
 
 function prepareTest(y){
@@ -31,25 +35,28 @@ function nextQues(){
     var d = document.getElementById("d");
     var x = 0;
     for (var i = 0; i < selected.length; i++){
-        if (i == current && current <= 4){
+        if (i == current && current <= (selected.length - 1)){
             pic.innerHTML = "<img class='picture' src='" + questions[selected[i]].pic + "'>";
-            a.innerHTML = questions[selected[i]].a;
-            b.innerHTML = questions[selected[i]].b;
-            c.innerHTML = questions[selected[i]].c;
-            d.innerHTML = questions[selected[i]].d;
+            a.innerHTML = "A.  " + questions[selected[i]].a;
+            b.innerHTML = "B.  " + questions[selected[i]].b;
+            c.innerHTML = "C.  " + questions[selected[i]].c;
+            d.innerHTML = "D.  " + questions[selected[i]].d;
         }
     }
-    current++;
-    if (current > 4){
+    if (current > (selected.length - 1)){
         pic.innerHTML = "";
         a.innerHTML = "";
         b.innerHTML = "";
         c.innerHTML = "";
         d.innerHTML = "";
         if (answers == response){
-            window.alert()
+            window.alert("All answers are right!");
+        }
+        if (answers !== response){
+            window.alert("Some were answered wrong!");
         }
     }
+    current++;
 }
 function storeQues(){
     sessionStorage.setItem("selected", selected);
