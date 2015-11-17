9195a6d7b803a8c3b73502f71597f12c4f38c6c0
Kris <mv9kjw@gmail.com>
2015-11-16 21:48:36 -0700

    Updated css and font, added modal to html and modal answer functionality to js file.

--- VisualArts/colorTheoryApp.js
+++ VisualArts/colorTheoryApp.js
@@ -1,15 +1,12 @@
-/**
- * Created by kris on 11/16/15.
- */
 
-var questionNumber, colorValue=['DarkOrange', 'purple', 'green', 'black'], answer, box1=['red', 'blue', 'yellow'], box2=['black', 'blue', 'yellow', 'red'], box1Color, box2Color, box1Value, box2Value;
+var questionNumber, colorValue=['DarkOrange', 'purple', 'green', 'black'], box1=['red', 'blue', 'yellow'], box2=['black', 'blue', 'yellow', 'red'], box1Color, box2Color, box1Value, box2Value, answer;
 
 function loadColors(n) {
     box1Value = Math.floor(Math.random() * 3);
     box2Value = (box1Value + 1);
     $('#box1').css("background-color", box1[box1Value]); box1Color=box1[box1Value];
-    $('#box2').css("background-color", box2[(box2Value)]); box2Color=box2[(box2Value)];
-    answer = colorValue[1];
+    $('#box2').css("background-color", box2[box2Value]); box2Color=box2[box2Value];
+
 
     $('#colorValue1').css("background-color", colorValue[0]);
     $('#colorValue2').css("background-color", colorValue[1]);
@@ -21,24 +18,55 @@ function loadColors(n) {
 function checkColor(n) {
     if( box2Color==box2[0] ) {
         if ( colorValue[n] == colorValue[3] ) {
-            console.log(true);
+            document.getElementById("answerModal").innerHTML = "<span class='rtxt'>" + "Correct!" + "</span>";
+            $('#answerModal').modal('show');
+            answer=true;
+
+        }
+        else {
+            document.getElementById("answerModal").innerHTML = "<span class='rtxt'>" + "Sorry, wrong answer. Try again!" + "</span>";
+            $('#answerModal').modal('show');
+            answer=false;
         }
     }
+    // if box1=RED & box2=BLUE then n should be purple
     if( box1Color==box1[0] && box2Color==box2[1] ) {
         if( colorValue[n] == colorValue[1] ) {
-            console.log(true);
+            document.getElementById("answerModal").innerHTML = "<span class='rtxt'>" + "Correct!" + "</span>";
+            $('#answerModal').modal('show');
+            answer=true;
+        }
+        else {
+            document.getElementById("answerModal").innerHTML = "<span class='rtxt'>" + "Sorry, wrong answer. Try again!" + "</span>";
+            $('#answerModal').modal('show');
+            answer=false;
         }
     }
     if( box1Color==box1[2] && box2Color==box2[3] ) {
         if( colorValue[n] == colorValue[0] ) {
-            console.log(true);
+            document.getElementById("answerModal").innerHTML = "<span class='rtxt'>" + "Correct!" + "</span>";
+            $('#answerModal').modal('show');
+            answer=true;
+        }
+        else {
+            document.getElementById("answerModal").innerHTML = "<span class='rtxt'>" + "Sorry, wrong answer. Try again!" + "</span>";
+            $('#answerModal').modal('show');
+            answer=false;
         }
     }
     if( box1Color==box1[1] && box2Color==box2[2] ) {
         if( colorValue[n] == colorValue[2] ) {
-            console.log(true);
+            document.getElementById("answerModal").innerHTML = "<span class='rtxt'>" + "Correct!" + "</span>";
+            $('#answerModal').modal('show');
+            answer=true;
+        }
+        else {
+            document.getElementById("answerModal").innerHTML = "<span class='rtxt'>" + "Sorry, wrong answer. Try again!" + "</span>";
+            $('#answerModal').modal('show');
+            answer=false;
         }
     }
+    if( answer == true ){ loadColors() }
 }
 
 function randomizer() {
