ec70012590da57936656a213aeb7f5e2b53176e3
edgarisla1 <edgar.isla.2013@gmail.com>
2015-11-18 10:07:27 -0700

    new

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -1,6 +1,7 @@
 /**
  * Created by gabedriggs1 on 11/18/15.
  */
+<<<<<<< Updated upstream
 function isEqual(){
     var answer = document.getElementById("eq").value;
     if (answer == "f(x)=2x"){
@@ -13,6 +14,22 @@ function isEqual(){
 }
 function isEqual2(){
     var answer2 = document.getElementById("eq1").value;
+    if (answer2 == "f(x)=3x"){
+=======
+
+function isEqual(){
+    var answer = $("#eq").val();
+    if (answer == "f(x)=2x"){
+>>>>>>> Stashed changes
+        alert("Nice you got it right!");
+    }
+    else
+    {
+        alert("Sorry! Try again :)");
+    }
+}
+function isEqual2(){
+    var answer2 = $("#eq1").val();
     if (answer2 == "f(x)=3x"){
         alert("Nice you got it right!");
     }
