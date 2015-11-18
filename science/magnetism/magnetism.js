02590717fd91c117b7ec7ece6b7d3c4b9c023944
Brandon O'Very <brovery@yahoo.com>
2015-11-18 09:16:27 -0700

    Trying to fix this crap.

---
+++ science/magnetism/magnetism.js
@@ -0,0 +1,37 @@
+/**
+ * Created by geoffbrown1 on 11/16/15.
+ */
+function resetActive(event, percent, step) {
+    $(".progress-bar").css("width", percent + "%").attr("aria-valuenow", percent);
+    $(".progress-completed").text(percent + "%");
+
+    $("div").each(function () {
+        if ($(this).hasClass("activestep")) {
+            $(this).removeClass("activestep");
+        }
+    });
+
+    if (event.target.className == "col-md-2") {
+        $(event.target).addClass("activestep");
+    }
+    else {
+        $(event.target.parentNode).addClass("activestep");
+    }
+
+    hideSteps();
+    showCurrentStepInfo(step);
+}
+
+function hideSteps() {
+    $("div").each(function () {
+        if ($(this).hasClass("activeStepInfo")) {
+            $(this).removeClass("activeStepInfo");
+            $(this).addClass("hiddenStepInfo");
+        }
+    });
+}
+
+function showCurrentStepInfo(step) {
+    var id = "#" + step;
+    $(id).addClass("activeStepInfo");
+}
