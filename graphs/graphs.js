83663399448cdda8d9076374278ca85b4edb9078
Gabriel King Driggs <gabe.driggs@gmail.com>
2015-11-18 08:53:56 -0700

    summary

---
+++ graphs/graphs.js
@@ -0,0 +1,13 @@
+/**
+ * Created by gabedriggs1 on 11/18/15.
+ */
+function isEqual(theirEquation,actualAnswer){
+    if (theirEquation == actualAnswer){
+        alert("Nice you got it right!");
+    }
+    else
+    {
+        alert("Sorry! Try again :)");
+    }
+}
+    
