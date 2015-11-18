8030870175e60a767785807da16c8df4acf09ddc
Gabriel King Driggs <gabe.driggs@gmail.com>
2015-11-18 09:17:38 -0700

    updates

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -1,9 +1,9 @@
 /**
  * Created by gabedriggs1 on 11/18/15.
  */
-var theirEquation1 = $("#eq1").valueOf();
+var theirEquation1 = $("#eq1").val();
 function isEqual(){
-    if (theirEquation1 == "f(x)="){
+    if (theirEquation1 == "f(x)=x"){
         alert("Nice you got it right!");
     }
     else
