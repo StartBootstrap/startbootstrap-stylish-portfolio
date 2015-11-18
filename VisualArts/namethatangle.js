2c6e710af6e6e169848d871ae7be8691dafb5626
ricardobonilla <ricardobonillawun@gmail.com>
2015-11-18 09:19:51 -0700

    Started input function

---
+++ VisualArts/namethatangle.js
@@ -0,0 +1,23 @@
+
+
+function Answer() {
+    var input1 = document.getElementById("input1").value;
+    //var getElementById("input2") = input2;
+    //var getElementById("input3") = input3;
+    //var getElementById("input4") = input4;
+    //var getElementById("input5") = input5;
+    //var getElementById("input6") = input6;
+
+    //var input1 = "right";
+    //var input2 = "acute";
+    //var input3 = "obtuse";
+    //var input4 = "acute";
+    //var input5 = "obtuse";
+    //var input6 = "right";
+
+    if (input1 === "right") {
+        alert("Correct");
+    } else {
+        alert("Wrong Answer");
+    }
+}
