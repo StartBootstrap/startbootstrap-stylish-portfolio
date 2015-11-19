e93c36d84410be3d91e49b550a0fc87b8ebbcdc4
Jonathan Bejarano <jbejar86@gmail.com>
2015-11-19 10:53:44 -0700

    Graphs.js fixing merge conflict

--- mathK12/graphs/graphs.js
+++ mathK12/graphs/graphs.js
@@ -1,7 +1,6 @@
 /**
  * Created by gabedriggs1 on 11/18/15.
  */
-<<<<<<< Updated upstream
 function isEqual(){
     var answer = $("#eq").val();
     if (answer == "f(x)=2x"){
@@ -12,22 +11,7 @@ function isEqual(){
         alert("Sorry! Try again :)");
     }
 }
-function isEqual2(){
-    var answer2 = $("#eq1").val();
-    if (answer2 == "f(x)=3x"){
-=======
 
-function isEqual(){
-    var answer = $("#eq").val();
-    if (answer == "f(x)=2x"){
->>>>>>> Stashed changes
-        alert("Nice you got it right!");
-    }
-    else
-    {
-        alert("Sorry! Try again :)");
-    }
-}
 function isEqual2(){
     var answer2 = $("#eq1").val();
     if (answer2 == "f(x)=3x"){
