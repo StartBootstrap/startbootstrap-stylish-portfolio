5becf814d68c37c0dc6eee6cad13093c17708dce
David Miller <hello@davidmiller.io>
2021-05-19 16:29:37 -0400

    BS5 initial update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,29 +9,6 @@
         $(this).toggleClass("active");
     });
 
-    // Smooth scrolling using anime.js
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-            this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length ?
-                target :
-                $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                anime({
-                    targets: 'html, body',
-                    scrollTop: target.offset().top,
-                    duration: 1000,
-                    easing: 'easeInOutExpo'
-                });
-                return false;
-            }
-        }
-    });
-
     // Closes responsive menu when a scroll trigger link is clicked
     $(".js-scroll-trigger").on('click', function () {
         $(".navbar-collapse").collapse("hide");
