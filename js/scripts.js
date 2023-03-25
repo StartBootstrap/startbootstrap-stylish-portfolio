f867a4c0b8836137efa7b9129c525f8b346fdc83
David Miller <hello@davidmiller.io>
2023-03-25 18:16:19 -0400

    Bootstrap 5.2.3 dependency update

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Stylish Portfolio v6.0.5 (https://startbootstrap.com/theme/stylish-portfolio)
-* Copyright 2013-2022 Start Bootstrap
+* Start Bootstrap - Stylish Portfolio v6.0.6 (https://startbootstrap.com/theme/stylish-portfolio)
+* Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
 */
 window.addEventListener('DOMContentLoaded', event => {
